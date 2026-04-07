3184617892dd299844a3751a36e9dfb086d78298
Dung Nguyen <158302785+DungxND@users.noreply.github.com>
2026-04-07 19:55:09 +0700

    hotfix(db): Change simplify eventStatus schema and use native bun-sql instead of node-postgres

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,21 +1,9 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/bun-sql';
 import * as schema from './schema';
 
 const connectionString = process.env.DATABASE_URL;
-
 if (!connectionString) {
   throw new Error('❌ DATABASE_URL không tồn tại trong file .env');
 }
 
-// Khởi tạo connection pool tới NeonDB
-// BẮT BUỘC dùng Pooled Connection String của Neon
-const pool = new Pool({
-  connectionString: connectionString,
-  max: 100,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
-
-// Export singleton instance của DB
-export const db = drizzle(pool, { schema });
+export const db = drizzle(connectionString, { schema });
