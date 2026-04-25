e099a809414250de49d808ad9f24c916e46cc4a2
DungxND <158302785+DungxND@users.noreply.github.com>
2026-04-25 21:51:31 +0700

    fix: format code correctly

--- src/routes/api/test-sse/+server.ts
+++ src/routes/api/test-sse/+server.ts
@@ -3,11 +3,11 @@ import { json } from '@sveltejs/kit';
 
 export const GET = async () => {
   console.log('[Test API] Đang bắn sự kiện từ Server...');
-  
-  eventBus.emit(SSE_EVENTS.SEAT_UPDATE(10), { 
-    seatId: 888, 
+
+  eventBus.emit(SSE_EVENTS.SEAT_UPDATE(10), {
+    seatId: 888,
     status: 'locked',
-    message: 'Chào bạn, mình bắn tin từ chính Server đây!' 
+    message: 'Chào bạn, mình bắn tin từ chính Server đây!',
   });
 
   return json({ message: 'Đã bắn tin nhắn thành công vào EventBus của Server!' });
